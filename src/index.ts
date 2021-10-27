11f62471bcd4f6a70af3092fd6ddd7a8a0ea7905
F <fatjonza@gmail.com>
2021-10-27 21:34:20 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,20 @@ process.env.TZ = 'Europe/Amsterdam'
 
 export const session = 
 {
-   ACCESS_TOKEN: 'ACCESS_TOKEN_HERE',
-   MIDNIGHT: new Date().setHours(24, 0, 0, 0)
+   ACCESS_TOKEN: 'Z0Zn4aBaAWktNgxK5uCeq0qV3kBsN5ImeY6ainpLcAc-VcWn0TeMaUHkDKRnHm8p',
+   MIDNIGHT: new Date().setHours(24, 0, 0, 0),
+   NEXT_UPDATE: new Date().setHours(new Date().getHours() + 1, 0, 0, 0)
 }
 
-schedule.scheduleJob('*/30 * * * *', () => updateAnnotation())
+schedule.scheduleJob('0 * * * *', () => updateAnnotation())
 
 const app = express()
-app.get('/', (req, res) => res.json({
-   TZ: process.env.TZ,
-   midnight: new Date(session.MIDNIGHT).toLocaleString('en-US'),
-   now: new Date().toLocaleString('en-US')
+app.get('/', (req, res) => res.json(
+{
+   timeZone: process.env.TZ,
+   dateNight: new Date(session.MIDNIGHT).toLocaleString('en-US'),
+   dateNext: new Date(session.NEXT_UPDATE).toLocaleString('en-US'),
+   dateNow: new Date().toLocaleString('en-US')
 }))
 
-app.listen(process.env.PORT || 3000, () => console.log('working'))
+app.listen(process.env.PORT || 3000, () => console.log('Application is running.'))
