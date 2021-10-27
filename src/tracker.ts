c08cf297596bb65056439b9fbfb3dac9ad0eef89
F <fatjonza@gmail.com>
2021-10-27 21:33:33 +0200

    Update tracker.ts

--- src/tracker.ts
+++ src/tracker.ts
@@ -1,75 +1,75 @@
-import fs from 'fs'
 import Axios from 'axios'
 import { session } from '.'
-import { prefix, toArray } from './config'
+import { getStatistics } from './tracker'
 
-export async function getStatistics(): Promise<string[]>
+const BODY = (statistics: string[]) =>
 {
-   const lastStats = require('../lastStats.json')
-   const users = toArray(require('../users.json'))
-   
-   const nowStats: any = {}
-   const output: string[] = []
+   return {
+      "annotation": { "body": { "markdown": statistics.join('\n') } }
+   }
+}
 
-   return new Promise<string[]>((resolveArray, rejectArray) => 
+export async function updateAnnotation()
+{
+   try
    {
-      const stats: any[] = [0, 0, 0, 0, 0, 0]
+      const statistics = await getStatistics()
       
-      Promise.all(users.map(id => 
+      for (const stat of statistics)
+         console.log(stat)
+
+      try
       {
-         return new Promise<void>(resolveUser => 
-         {
-            Axios.get(`https://api.genius.com/users/${id}`,
+         const response = await Axios.put(
+            'https://api.genius.com/annotations/20112143', BODY([lastUpdate(), volgendeUpdate(), ...statistics]),
             {
                headers: { Authorization: `Bearer ${session.ACCESS_TOKEN}` }
-            })
-            .then((response: any) => 
-            {      
-               const json = response.data.response.user
-                  
-               stats[0] += json.stats.transcriptions_count
-               stats[1] += json.stats.annotations_count
-               stats[2] += json.stats.answers_count
-               stats[3] += json.stats.questions_count
-               stats[4] += json.stats.forum_posts_count
-               stats[5] += json.iq
-            })
-            .then(() => resolveUser())
-         })
-      }))
-      .then(() => 
+            }
+         )
+
+         console.log(`\nResponse status code: ${response.status}`)
+         console.log('Annotation successfully updated!')      
+      } 
+      catch (error: any)
       {
-         for (let i = 0; i < prefix.length; i++)
-         {
-            const yesterday = lastStats[i][0]
-            const today = stats[i] - lastStats[i][1]
+         const meta = error.response.data.meta
 
-            const evolution = ((today - yesterday) / yesterday) * 100
-            let evolutionString = `(${(evolution  > 0 ? '+' : '') + Math.round(evolution)}%)`
+         console.log(`\nResponse status code: ${meta.status}`)
+         console.log(`Message: ${meta.message}`)
+      }
+   }
+   catch (error: any) 
+   {
+      console.error('Couldn\'t get the statistics of the community.', error)
+   }
+}
 
-            if (evolution === Infinity) evolutionString = '(+100%)'
-            if (evolution === -Infinity) evolutionString = ''
-            if (isNaN(evolution)) evolutionString = ''
-            
-            output.push(' ')
-            output.push(`${prefix[i]}: **${stats[i].toLocaleString('en-US')} totaal**`)
-            output.push(`<small> • Gisteren: ${yesterday.toLocaleString('en-US')}</small>`)
-            output.push(` • **Vandaag**: ${today.toLocaleString('en-US')} ${evolutionString}`)
+const lastUpdate = (): string =>
+{
+   const date = new Date()
+   
+   .toLocaleString('nl-BE', DATE_FORMAT
+   ).replace(' ', '</b> om <b>')
 
-            nowStats[i] = [today, stats[i]]
-         }
-      })
-      .then(() => 
-      {
-         const now = new Date()
-      
-         if (now.getTime() >= session.MIDNIGHT)
-         {
-            fs.writeFileSync('lastStats.json', JSON.stringify(nowStats))
-            session.MIDNIGHT = now.setHours(24, 0, 0, 0)
-         }
-      })
-      .then(() => resolveArray(output))
-      .catch(error => rejectArray(error))
-   })
+   return `<small>Laatst geüpdatet: <b>${date}</b></small>`
+}
+
+const volgendeUpdate = (): string =>
+{
+   const dateNext = new Date(new Date().getTime() + 60 * 60 * 1000)
+   session.NEXT_UPDATE = dateNext.getTime()
+
+   const date = dateNext
+   .toLocaleString('nl-BE', DATE_FORMAT
+   ).replace(' ', '</b> om <b>')
+
+   return `<small>Volgende update: <b>${date}</b></small>`
+}
+
+const DATE_FORMAT: any = { 
+   year: '2-digit', 
+   month: '2-digit', 
+   day: '2-digit', 
+   hour: '2-digit', 
+   minute: '2-digit'
 }
